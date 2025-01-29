049e0841e0d0e58168e680c71f6ad4e0e41d0deb
AyoIbitola <mremmatola@gmail.com>
2025-01-29 21:27:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from datetime import datetime, timezone
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
+@app.get("/")
+def user_info():
+    current_datetime = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    return{
+        "email" : "mremmatola@gmail.com",
+        "current_datetime":current_datetime,
+        "github_url":"https://github.com/AyoIbitola/hng-stage-0"
+    }
